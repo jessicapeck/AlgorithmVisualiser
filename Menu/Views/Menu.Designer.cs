07ec440d7659bb5a9c745fc09883815b814b5056
jessicapeck <37042385+jessicapeck@users.noreply.github.com>
2023-01-05 09:03:11 +0000

    created menu UI

--- Menu/Views/Menu.Designer.cs
+++ Menu/Views/Menu.Designer.cs
@@ -29,69 +29,162 @@ namespace Prototype
         /// </summary>
         private void InitializeComponent()
         {
-            this.welcome_label = new System.Windows.Forms.Label();
             this.sorting_algorithm_animations_button = new System.Windows.Forms.Button();
             this.algorithm_information_button = new System.Windows.Forms.Button();
             this.interactive_algorithms_button = new System.Windows.Forms.Button();
             this.shortest_path_algorithms_button = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // welcome_label
-            // 
-            this.welcome_label.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.welcome_label.BackColor = System.Drawing.Color.Transparent;
-            this.welcome_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.welcome_label.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.welcome_label.Location = new System.Drawing.Point(12, 25);
-            this.welcome_label.Name = "welcome_label";
-            this.welcome_label.Size = new System.Drawing.Size(659, 64);
-            this.welcome_label.TabIndex = 0;
-            this.welcome_label.Text = "Welcome to the Algorithm Visualiser!";
-            this.welcome_label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // sorting_algorithm_animations_button
             // 
             this.sorting_algorithm_animations_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.sorting_algorithm_animations_button.BackColor = System.Drawing.Color.LightSkyBlue;
             this.sorting_algorithm_animations_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sorting_algorithm_animations_button.Location = new System.Drawing.Point(32, 141);
+            this.sorting_algorithm_animations_button.Location = new System.Drawing.Point(17, 121);
             this.sorting_algorithm_animations_button.Name = "sorting_algorithm_animations_button";
             this.sorting_algorithm_animations_button.Size = new System.Drawing.Size(279, 63);
             this.sorting_algorithm_animations_button.TabIndex = 1;
             this.sorting_algorithm_animations_button.Text = "Sorting Algorithm Animations";
-            this.sorting_algorithm_animations_button.UseVisualStyleBackColor = true;
+            this.sorting_algorithm_animations_button.UseVisualStyleBackColor = false;
             // 
             // algorithm_information_button
             // 
             this.algorithm_information_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.algorithm_information_button.BackColor = System.Drawing.Color.LightSkyBlue;
             this.algorithm_information_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.algorithm_information_button.Location = new System.Drawing.Point(358, 141);
+            this.algorithm_information_button.Location = new System.Drawing.Point(17, 291);
             this.algorithm_information_button.Name = "algorithm_information_button";
             this.algorithm_information_button.Size = new System.Drawing.Size(279, 63);
             this.algorithm_information_button.TabIndex = 2;
             this.algorithm_information_button.Text = "Facts and Information";
-            this.algorithm_information_button.UseVisualStyleBackColor = true;
+            this.algorithm_information_button.UseVisualStyleBackColor = false;
             // 
             // interactive_algorithms_button
             // 
             this.interactive_algorithms_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.interactive_algorithms_button.BackColor = System.Drawing.Color.LightSkyBlue;
             this.interactive_algorithms_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.interactive_algorithms_button.Location = new System.Drawing.Point(32, 249);
+            this.interactive_algorithms_button.Location = new System.Drawing.Point(17, 206);
             this.interactive_algorithms_button.Name = "interactive_algorithms_button";
             this.interactive_algorithms_button.Size = new System.Drawing.Size(279, 63);
             this.interactive_algorithms_button.TabIndex = 3;
             this.interactive_algorithms_button.Text = "Interactive Sorting Algorithms";
-            this.interactive_algorithms_button.UseVisualStyleBackColor = true;
+            this.interactive_algorithms_button.UseVisualStyleBackColor = false;
             // 
             // shortest_path_algorithms_button
             // 
             this.shortest_path_algorithms_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.shortest_path_algorithms_button.BackColor = System.Drawing.Color.LightSkyBlue;
             this.shortest_path_algorithms_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.shortest_path_algorithms_button.Location = new System.Drawing.Point(358, 249);
+            this.shortest_path_algorithms_button.Location = new System.Drawing.Point(17, 376);
             this.shortest_path_algorithms_button.Name = "shortest_path_algorithms_button";
             this.shortest_path_algorithms_button.Size = new System.Drawing.Size(279, 63);
             this.shortest_path_algorithms_button.TabIndex = 4;
             this.shortest_path_algorithms_button.Text = "Shortest Path Algorithms";
-            this.shortest_path_algorithms_button.UseVisualStyleBackColor = true;
+            this.shortest_path_algorithms_button.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(681, 81);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(466, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Algorithm Visualiser";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(5, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(338, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Welcome to the Algorithm Visualiser!";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumPurple;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(313, 121);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(347, 319);
+            this.panel2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.MediumPurple;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(5, 51);
+            this.label3.MaximumSize = new System.Drawing.Size(340, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(339, 40);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Compare different sorting algorithms using the \'Sorting Algorithm Animations\' fea" +
+    "ture.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.MediumPurple;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(5, 111);
+            this.label4.MaximumSize = new System.Drawing.Size(340, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(305, 60);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Test your knowledge of sorting algorithms using the \'Interactive Sorting Algorith" +
+    "ms\' feature.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.MediumPurple;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(6, 190);
+            this.label5.MaximumSize = new System.Drawing.Size(340, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(318, 40);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Find information about algorithms using the \'Facts and Information\' feature.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.MediumPurple;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(321, 378);
+            this.label6.MaximumSize = new System.Drawing.Size(340, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(332, 40);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "See visualisations of shortest path algorithms using the \'Shortest Path Algorithm" +
+    "s\' feature.";
             // 
             // Menu
             // 
@@ -100,27 +193,40 @@ namespace Prototype
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.Azure;
-            this.ClientSize = new System.Drawing.Size(678, 369);
+            this.ClientSize = new System.Drawing.Size(452, 328);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.shortest_path_algorithms_button);
             this.Controls.Add(this.interactive_algorithms_button);
             this.Controls.Add(this.algorithm_information_button);
             this.Controls.Add(this.sorting_algorithm_animations_button);
-            this.Controls.Add(this.welcome_label);
             this.MinimizeBox = false;
             this.Name = "Menu";
             this.Text = "Algorithm Visualiser";
             this.Load += new System.EventHandler(this.Menu_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label welcome_label;
         private System.Windows.Forms.Button sorting_algorithm_animations_button;
         private System.Windows.Forms.Button algorithm_information_button;
         private System.Windows.Forms.Button interactive_algorithms_button;
         private System.Windows.Forms.Button shortest_path_algorithms_button;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
     }
 }
 
